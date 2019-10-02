b6c8f19669e01f86059344d2462f7857f5fca4cb
Habsu <habucea@gmail.com>
2019-10-02 14:17:14 +0100

    more refactoring

--- app.js
+++ app.js
@@ -2,32 +2,30 @@
 function ValidateIpv4Address(string) {
     stringIntoArray = string.split('.')
     lastElement = stringIntoArray.length - 1
-    if (arrayLength(stringIntoArray) || areElementsValid(stringIntoArray) || isLastElementValid(stringIntoArray))
-        return false;
-    else
-        return true;
+    return (isCorrectLength(stringIntoArray) && isLastElementValid(stringIntoArray) && isAnyElementIsValid(stringIntoArray))
 }
 
-function arrayLength(stringIntoArray) {
-    return stringIntoArray.length !== 4
+function isCorrectLength(stringIntoArray) {
+    return stringIntoArray.length === 4
 }
 
-function isLastElemetZero(stringIntoArray) {
-    return stringIntoArray[lastElement] === '0'
+function isLastElemetNotZero(stringIntoArray) {
+    return stringIntoArray[lastElement] !== '0'
 }
-function isLastElement255(stringIntoArray) {
-    return stringIntoArray[lastElement] === '225'
+function isLastElementNot255(stringIntoArray) {
+    return stringIntoArray[lastElement] !== '225'
 }
 
 function isLastElementValid(stringIntoArray) {
-    return isLastElement255(stringIntoArray) || isLastElemetZero(stringIntoArray)
+    return isLastElementNot255(stringIntoArray) && isLastElemetNotZero(stringIntoArray)
 }
 
-function areElementsValid(stringIntoArray) {
+function isAnyElementIsValid(stringIntoArray) {
     for (var i = 0; i < stringIntoArray.length; i++) {
         var elementRange = parseInt(stringIntoArray[i], 10);
         if (elementRange > 225 || elementRange < 0)
-            return true;
+            return false;
     }
+    return true;
 }
 module.exports = ValidateIpv4Address;
