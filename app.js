1f2e4d6d1244621364faeaa62f2933236c476351
Habsu <habucea@gmail.com>
2019-10-02 14:36:07 +0100

    more refactoring - Final Final

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
 function ValidateIpv4Address(string) {
     stringIntoArray = string.split('.');
-    lastElement = stringIntoArray.length - 1;
-    return isCorrectLength() && isLastElementValid() && isAnyElementIsValid()
+    lastElementIndex = stringIntoArray.length - 1;
+    return isCorrectLength() && islastElementIndexValid() && isAnyElementIsValid()
 }
 
 function isCorrectLength() {
-    return lastElement === 3;
+    return lastElementIndex === 3;
 }
 
 function isLastElemetNotZero() {
-    return stringIntoArray[lastElement] !== '0';
+    return stringIntoArray[lastElementIndex] !== '0';
 }
 
-function isLastElementNot255() {
-    return stringIntoArray[lastElement] !== '225';
+function islastElementIndexNot255() {
+    return stringIntoArray[lastElementIndex] !== '225';
 }
 
-function isLastElementValid() {
-    return isLastElementNot255() && isLastElemetNotZero();
+function islastElementIndexValid() {
+    return islastElementIndexNot255() && isLastElemetNotZero();
 }
 
 function isAnyElementIsValid() {
     for (var i = 0; i < stringIntoArray.length; i++) {
-        var elementRange = parseInt(stringIntoArray[i], 10);
-        if (elementRange > 225 || elementRange < 0)
+        var stringNumberIntoInteger = parseInt(stringIntoArray[i], 10);
+        if (stringNumberIntoInteger > 225 || stringNumberIntoInteger < 0)
             return false;
     }
     return true;
